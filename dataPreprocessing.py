7fa18b670072803a6ce01c8270635915ce6dfd68
Benjamin Moore <bcmoor11@asu.edu>
2022-10-27 02:59:14 -0700

    Added in side effect data.

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('Datasets/meddra_all_indications.tsv', header=None, sep='\t')
+df1 = pd.read_csv('Datasets/meddra_all_indications.tsv', header=None, sep='\t')
 
 #print(df)
 
-df = df.drop([0, 2, 4, 5, 6], axis=1)
-df = df.drop_duplicates(subset=[1])
+df1 = df1.drop([0, 2, 4, 5, 6], axis=1)
+df1 = df1.drop_duplicates(subset=[1])
 
-print(df)
+df1.to_csv('NewDatasets/indications.csv', header=None, index=None)
 
+df2 = pd.read_csv('Datasets/meddra_all_se.tsv', header=None, sep='\t')
 
+df2 = df2.drop([0,1,2,3], axis=1)
+df2 = df2.drop_duplicates(subset=[4])
 
-df.to_csv('NewDatasets/indications.csv', header=None, index=None)
+df1.to_csv('NewDatasets/sideEffects.csv', header=None, index=None)
+
+print(df2)
---
+++ insertingSideEffects.py
@@ -0,0 +1,21 @@
+from cv2 import dnn_DetectionModel
+import pandas as pd
+import psycopg2
+
+conn = psycopg2.connect("host=localhost dbname=postgres user=newuser password=password")
+
+cur = conn.cursor()
+
+df = pd.read_csv('NewDatasets/sideEffects.csv', header=None)
+cur.execute("CREATE TABLE SideEffects (seId text PRIMARY KEY, seName text);")
+for i in range(0, len(df)):
+    seId = df.iloc[i,0]
+    seName = df.iloc[i,1]
+    print("{0} : {1}".format(seId, seName))
+    cur.execute("INSERT INTO SideEffects (seId, seName) VALUES ('{0}', '{1}');".format(seId, seName))
+
+conn.commit()
+
+# Close communication with the database
+cur.close()
+conn.close()
