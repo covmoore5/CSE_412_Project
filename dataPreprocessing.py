382b0df99f632e48ca400836c8586f90ca911380
Benjamin Moore <bcmoor11@asu.edu>
2022-10-27 17:05:29 -0700

    Did the inserting od the drug_se information

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -13,8 +13,12 @@ df2 = pd.read_csv('Datasets/meddra_all_se.tsv', header=None, sep='\t')
 
 df2 = df2.drop([0,1,2,3], axis=1)
 df2 = df2.drop_duplicates(subset=[4])
-
-df1.to_csv('NewDatasets/sideEffects.csv', header=None, index=None)
+df2 = df2.rename(columns={4:0, 5:1})
+#print(df2)
+sideEffect = df2.to_dict()
+#print(df2)
+#print(sideEffect)
+df2.to_csv('NewDatasets/sideEffects.csv', header=None, index=None)
 
 #print(df2)
 
@@ -24,15 +28,30 @@ df3 = df3.drop([2,3,4,5,6], axis=1)
 df3 = df3.drop_duplicates()
 df4 = pd.read_csv('Datasets/drug_names.tsv', header=None, sep='\t')
 
-df4 = df4.drop([1], axis=1)
+df4 = df4.drop([0], axis=1)
 drugs = df4.to_dict()
 df3.to_csv('NewDatasets/treats.csv', header=None, index=None)
 
 df5 = pd.read_csv('Datasets/meddra_all_se.tsv', header=None, sep='\t')
-df5 = df5.drop([1,2,3,5], axis=1)
-df5 = df5.drop_duplicates(subset=[0])
+df5 = df5.drop([1,3,4,5], axis=1)
+#print(df5)
+temp = {'drugId': [], 'seId': []}
+for i in range(len(df5)):
+    #print(df5.iloc[i, 2])
+    if df5.iloc[i, 1] in sideEffect[0].values():
+        #print(df5.iloc[i, 0], df5.iloc[i, 1])
+        temp['drugId'].append(str(df5.iloc[i, 0]))
+        temp['seId'].append(str(df5.iloc[i, 1]))
 
-df5.to_csv('NewDatasets/drug_se.csv', header=None, index=None)
+#print(temp)
 
+df6 = pd.DataFrame(temp, index=None)
+df6 = df6.drop_duplicates()
+        
+#print(df6)
+        
+df6.to_csv('NewDatasets/drug_se.csv', header=None, index=None)
 
-print(df5)
+if 'C0002418' in sideEffect[0].values():
+     print("true")
+#print(sideEffect[0])
